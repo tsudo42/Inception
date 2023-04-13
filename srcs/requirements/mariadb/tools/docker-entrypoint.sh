75d37ee65d73fac99d644cb6b3f2d47dcbf0b764
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2023-04-14 02:29:27 +0900

    CHANGE: security

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -2,18 +2,26 @@
 
 set -e
 
-# c.f. --init-file /path/to/bootstrap.sql
+# Initialize database
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing MariaDB database..."
 
-mariadbd --user=mysql --bootstrap << EOF
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
+    chown -R mysql:mysql /var/lib/mysql
+
+    mariadbd --user=mysql --bootstrap << EOF
 USE mysql;
 FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS ${MYSQL_WORDPRESS_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED VIA mysql_native_password USING PASSWORD('${MYSQL_PASSWORD}');
-GRANT ALL PRIVILEGES ON wordpress.* TO '${MYSQL_USER}'@'%';
+GRANT ALL PRIVILEGES ON ${MYSQL_WORDPRESS_DATABASE}.* TO '${MYSQL_USER}'@'%';
 
 FLUSH PRIVILEGES;
 EOF
+
+    echo "MariaDB database initialized."
+fi
 
 exec "$@"
--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -3,8 +3,19 @@
 set -e
 
 if [ ! -e wp-config.php ]; then
-	wp config create --dbname="${MYSQL_WORDPRESS_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb
-	wp core install --url="${DOMAIN_NAME}" --title="${WORDPRESS_TITLE}" --admin_user="${WORDPRESS_ADMIN}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --skip-email
+    wp config create \
+        --dbname="${MYSQL_WORDPRESS_DATABASE}" \
+        --dbuser="${MYSQL_USER}" \
+        --dbpass=${MYSQL_PASSWORD} \
+        --dbhost=mariadb
+
+    wp core install \
+        --url="https://${DOMAIN_NAME}" \
+        --title="${WORDPRESS_TITLE}" \
+        --admin_user="${WORDPRESS_ADMIN}" \
+        --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+        --admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+        --skip-email
 fi
 
 exec "$@"
