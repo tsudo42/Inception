7e4ab54b34650e1370ad34ef54931cc25c6b09b4
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2023-04-16 03:28:09 +0900

    CHANGE: nginx ssl certificate

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -4,6 +4,21 @@ set -e
 
 sed -i -r "s/\\$\\{DOMAIN_NAME\\}/${DOMAIN_NAME}/g" /etc/nginx/conf.d/default.conf
 
+# if [ ! -e /etc/nginx/ssl/nginx.pem ]; then
+if [ true ]; then
+    mkdir -p /etc/nginx/ssl
+
+    openssl req -new -x509 \
+        -sha256 -newkey rsa:2048 -days 365 -nodes \
+        -subj /CN=${DOMAIN_NAME} \
+        -out /etc/nginx/ssl/nginx.pem \
+        -keyout /etc/nginx/ssl/nginx.pem
+
+    chown root:root -R /etc/nginx/ssl
+    chmod 600 /etc/nginx/ssl/*
+    chmod 700 /etc/nginx/ssl
+fi
+
 echo "Starting nginx..."
 
 exec "$@"
